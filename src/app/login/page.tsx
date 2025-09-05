93eed4dc4c6aaf15e43e20b53acba3a25a0bf52e
pawel <pawiercinski@gmail.com>
2025-09-05 14:52:06 +0200

    restore login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Login from "@/components/pages/Login";
+
+const loginPage = () => {
+  return (
+    <>
+      <Login />
+    </>
+  );
+};
+export default loginPage;
