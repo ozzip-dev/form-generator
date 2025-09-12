105d521ac11e91a2bb1286133d9c20332edcf134
pawel <pawiercinski@gmail.com>
2025-09-12 22:49:59 +0200

    display created form input blocks

---
+++ src/components/form/CreateFormField.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { FormInput } from "@/types/input";
+
+function CreateFormField(props: FormInput) {
+  console.log(props);
+
+  const { description, type, header, required } = props;
+  return (
+    <>
+      <div className="w-96 h-28 border border-black p-2">
+        <div>
+          <b>{header}</b> {""} ({type})
+        </div>
+        <div>{description}</div>
+        {required && "Required"}
+      </div>
+    </>
+  );
+}
+
+export default CreateFormField;
--- src/components/pages/EditForm.tsx
+++ src/components/pages/EditForm.tsx
@@ -4,9 +4,11 @@ import { findOne } from "@/lib/mongo";
 import { formatDate } from "@/lib/utils";
 import { Form } from "@/types/form";
 import { FormInput } from "@/types/input";
+import CreateFormField from "../form/CreateFormField";
 
 type Props = { form: Form };
 
+// TODO Pawel: why rendered twice?
 const EditForm = (props: Props) => {
   const { title, description, inputs, createdAt, updatedAt } = props.form;
   return (
@@ -15,8 +17,12 @@ const EditForm = (props: Props) => {
       <div>Edytowano: {formatDate(new Date(updatedAt))}</div>
       <div className="text-5xl">{title}</div>
       <div className="text-3xl">{description}</div>
-      <div>
-        ({inputs.map((el: FormInput, i: number) => el.header).join(", ")})
+      <div className="mt-6 flex flex-col gap-4">
+        {inputs
+          .sort((a, b) => a.order - b.order)
+          .map((el: FormInput, i: number) => (
+            <CreateFormField {...el} key={i} />
+          ))}
       </div>
     </div>
   );
