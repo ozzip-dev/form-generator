17ef0f905b3482113caaf1a43c847cffbccb1d04
pawel <pawiercinski@gmail.com>
2025-09-18 13:22:59 +0200

    fix: create empty form

--- src/actions/form/CreateDraft.tsx
+++ src/actions/form/CreateDraft.tsx
@@ -8,22 +8,29 @@ import { auth } from "@/lib/auth";
 import { Document, ObjectId } from "mongodb";
 import { headers } from "next/headers";
 
+// TODO: move out
+const isEmpty = (templateId: string) => templateId === 'empty'
+
 export async function CreateDraft(templateId: string) {
   const session = await auth.api.getSession({
     headers: await headers(), // you need to pass the headers object.
   });
 
+  const empty = isEmpty(templateId)
+
   const template: Document | null = await findOne(db, "form", {
     id: templateId,
   });
 
-  if (!template || !session?.user) {
+  if ((!template && !empty) || !session?.user) {
     // TODO: add error message
     console.error("Invalid template or you got logged out");
     return;
   }
 
-  const { title, description, inputs } = template as Form;
+  const { title, description, inputs } = empty 
+    ? { title: '[ tytuł ]', description: '[ opis ]', inputs: []}
+    : template as Form;
 
   const id: ObjectId = await createDraft(
     db,
