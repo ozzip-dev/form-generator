1de82a0877ba28e8a749aa4b80a0f67f0ec1d566
pawel <pawiercinski@gmail.com>
2025-09-26 05:20:03 +0200

    form types refactor

--- src/types/form.d.ts
+++ src/types/form.d.ts
@@ -4,31 +4,29 @@ import { FormInput } from "./input";
 export type FormState = "draft" | "active" | "disabled" | "template";
 
 export interface FormTemplate extends Document {
-  _id?: ObjectId,
-  id?: string, // TODO Pawel: id + state 'template': too much?
-  title?: string,
-  description?: string,
+  _id?: ObjectId
+  id?: string // TODO Pawel: id + state 'template': too much?
+  title?: string
+  description?: string
   inputs: FormInput[] 
 }
 
-// TODO Pawel: divide into FormInsert etc.
-export interface Form extends FormTemplate {
-  createdBy?: ObjectId // if template form, no author needed
-  createdAt: Date
-  updatedAt: Date
+// TODO: think of better name for non-template forms
+interface FormCreated extends FormTemplate {
   url?: string // or urls: string[] ? maybe one's enough
   state?: FormState
 }
 
-export interface FormSerialized {
+// TODO Pawel: divide into FormInsert etc.
+export interface Form extends FormCreated {
+  createdBy?: ObjectId // if template form, no author needed
+  createdAt: Date
+  updatedAt: Date
+}
+
+export interface FormSerialized extends FormCreated {
   _id?: string
-  id?: string
-  title?: string
-  description?: string
-  inputs: FormInput[] 
   createdBy?: string
   createdAt: string
   updatedAt: string
-  url?: string
-  state?: FormState
 }
