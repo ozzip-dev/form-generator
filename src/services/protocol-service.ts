6f117fa84bb500c873e09f7760e3889ce2bd3407
pawelwier <pawiercinski@gmail.com>
2025-10-27 21:22:32 +0100

    add getProtocolsNoData function

--- src/services/protocol-service.ts
+++ src/services/protocol-service.ts
@@ -1,9 +1,19 @@
-import { Db } from "mongodb";
-import { findAll, insert } from "@/lib/mongo";
+import { Collection, Db, Document } from "mongodb";
+import { findAll, getCollection, insert } from "@/lib/mongo";
 import { Protocol } from "@/types/protocol";
 
 export async function getProtocols(database: Db): Promise<Protocol[]> {
   const protocols = await findAll(database, "protocol");
+  return protocols as Protocol[];
+}
+
+// TODO Pawel: get records with no data, add download action
+export async function getProtocolsNoData(database: Db): Promise<Protocol[]> {
+  const collection: Collection<Document> = getCollection(database, 'protocol');
+  const protocols = await collection
+    .find({})
+    .project({ data: 0 })
+    .toArray()
   return protocols as Protocol[];
 }
 
