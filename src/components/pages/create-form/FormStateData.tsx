474da462e9be6777d576dfe21f8c61b34bc8faa2
pawelwier <pawiercinski@gmail.com>
2025-10-30 08:38:38 +0100

    add form url info

--- src/components/pages/create-form/FormStateData.tsx
+++ src/components/pages/create-form/FormStateData.tsx
@@ -11,23 +11,22 @@ type Props = {
 // TODO: Tymczasowy komponent, na bank to będzie inaczej
 const FormStateData = ({ form }: Props) => {
   const isStateDraft: boolean = isDraft(form)
-  const formUrl: string = form.url ?? `${window.location.origin}/submit/${form._id}`
+  const formUrl = `/${form.url || `submit/${form._id}`}`
 
   return (
       isStateDraft ? (
-        <div className="flex justify-end">
-          Formularz jest szkicem. <PublishFormButton form={form} />
+        <div className="w-fit flex items-center gap-3 *:flex-shrink-0">
+          <div>
+            Formularz jest szkicem.
+          </div>
+          <PublishFormButton form={form} />
         </div>
       ) : (
         <div className="flex gap-2 m-4">
           <div>
             Opublikowano:
           </div>
-          <a
-            href={formUrl}
-            target="_blank"
-            className="font-bold"
-          >{formUrl}</a>
+            <div className="font-bold">{formUrl}</div>
         </div>
       )
   );
