adee4848b3485bc5326f6828adaf4f19eac6ac77
krzysztofe <gitaragra@gmai.com>
2025-10-30 15:17:13 +0100

    Add try catch to EditInputTypeAction

--- src/actions/edit-form/EditInputTypeAction.tsx
+++ src/actions/edit-form/EditInputTypeAction.tsx
@@ -6,17 +6,23 @@ import { revalidateTag } from "next/cache";
 import { checkFormHasInputWithId } from "../utils";
 import { updateFormInputType } from "@/services/input-service";
 import { InputType } from "@/enums/input";
+import { requireUser } from "@/dataAccessLayer/queries";
 
 export async function EditInputTypeAction(
   formIdString: string,
   inputId: string,
   type: InputType
 ): Promise<void> {
+  requireUser();
   const formId = new ObjectId(formIdString);
 
   if (!checkFormHasInputWithId(db, formId, inputId)) return;
+  try {
+    await updateFormInputType(db, formId, inputId, type);
 
-  await updateFormInputType(db, formId, inputId, type);
-
-  revalidateTag(`form-${formId}`);
+    revalidateTag(`form-${formId}`);
+  } catch (err) {
+    console.error("Błąd EditInputTypeAction:", err);
+    throw new Error(`Błąd: ${err}`);
+  }
 }
