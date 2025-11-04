a732c10f8d7735ea0ecdc205955f204112fd6ac7
pawelwier <pawiercinski@gmail.com>
2025-11-04 20:46:38 +0100

    add form url settings to edit form; minor refactor

---
+++ src/components/pages/create-form/CreatedUpdatedInfo.tsx
@@ -0,0 +1,20 @@
+import { formatDateAndHour } from "@/helpers/dates/formatDateAndHour";
+
+type Props = {
+  createdAt: string
+  updatedAt: string
+}
+
+export default function CreatedUpdatedInfo({ createdAt, updatedAt }: Props) {
+
+  return (
+    <div className="flex justify-between">
+      <div className="text-xs text-gray-400 mt-1">
+        Edytowano: {formatDateAndHour(updatedAt)}
+      </div>
+      <div className="text-xs text-gray-400 mt-1">
+        Utworzono: {formatDateAndHour(createdAt)}
+      </div>
+    </div>
+  );
+}
