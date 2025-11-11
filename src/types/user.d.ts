edea22bb3d84635fbf8e2f0f67066d910a5b65d2
pawelwier <pawiercinski@gmail.com>
2025-11-11 18:16:51 +0100

    add user type comment

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -8,6 +8,7 @@ export type UserCommitteeInfo = Record<CommitteeInfoKey, string>
 
 type UserWithCommittee = User & UserCommitteeInfo
 
+// TODO Pawel: infer addition fields globally, edit IUser => User
 export interface IUser extends Document, UserWithCommittee {
   role: UserRole;
 }
