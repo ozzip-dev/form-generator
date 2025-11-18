0da5f64e5fb90de5455758adaa71974e61ff9997
krzysztofe <gitaragra@gmai.com>
2025-11-18 23:29:25 +0100

    Add number validation in createdFormSchema

--- src/components/pages/view/CreatedForm.tsx
+++ src/components/pages/view/CreatedForm.tsx
@@ -1,6 +1,241 @@
+// "use client";
+
+// import { SubmitForm } from "@/actions/form/SubmitForm";
+// import {
+//   Button,
+//   CheckboxGroupField,
+//   InputFields,
+//   RadioGroupField,
+//   TextareaFields,
+// } from "@/components/shared";
+// import { useToast } from "@/hooks/useToast";
+// import { uniqueErrorMessage } from "@/lib/error";
+// import { createdFormSchema } from "@/lib/zodSchema/createdFormSchema";
+// import { FormSerialized } from "@/types/form";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { FormInput } from "lucide-react";
+// import { ObjectId } from "mongodb";
+// import { useEffect } from "react";
+// import { FormProvider, useForm } from "react-hook-form";
+
+// type Props = {
+//   form: FormSerialized;
+//   isPreview?: boolean;
+// };
+
+// const CreatedForm = (props: Props) => {
+//   const {
+//     form: { title, description, inputs },
+//     isPreview = false,
+//   } = props;
+//   const schema = createdFormSchema(props.form.inputs);
+//   const { toast } = useToast();
+
+//   const defaultValues = inputs.reduce((acu: any, input) => {
+//     const { header, type, options, id } = input;
+//     const inputLabel = header;
+
+//     const inputOptions = options.reduce(
+//       (acu: Record<string, boolean>, option: string) => {
+//         acu[option] = false;
+
+//         return acu;
+//       },
+//       {}
+//     );
+
+//     const checkboxOptions = (acu[inputLabel] =
+//       type === "checkbox" ? inputOptions : "");
+//     return acu;
+//   }, {});
+
+//   const methods = useForm({
+//     defaultValues,
+//     resolver: zodResolver(schema),
+//     mode: "all",
+//   });
+
+//   const {
+//     register,
+//     reset,
+//     formState: { errors },
+//     trigger,
+//     control,
+//     setError,
+//     watch,
+//     handleSubmit,
+//   } = methods;
+
+//   // useEffect(() => {
+//   //   const subscription = watch((values) => {
+//   //     console.log("Aktualne wartości:", values);
+//   //   });
+//   //   return () => subscription.unsubscribe();
+//   // }, [watch]);
+
+//   const onSubmit = async (data: any) => {
+//     const _id = props.form._id?.toString();
+//     if (!_id) return; // ?
+
+//     // TODO: czemu trafiają tu Labele/Headery? Usunąć z obiektu i usunąć ten kod
+//     const keys = Object.keys(data);
+//     const inputIds = inputs.map(({ id }) => id);
+//     keys.forEach((key) => {
+//       if (!inputIds.includes(key)) delete data[key];
+//     });
+
+//     try {
+//       await SubmitForm(_id, data);
+//       toast({
+//         title: "Sukces! Dzieki",
+//         variant: "success",
+//       });
+//     } catch (e) {
+//       console.log("blad ", e);
+//       const err = e as Error;
+//       const title =
+//         err.message == uniqueErrorMessage
+//           ? "Formularz z podanymi danymi zostal juz wyslany. Skontaktuj sie z administratorem."
+//           : "Blad. Sprobuj ponownie.";
+//       toast({
+//         title,
+//         variant: "error",
+//       });
+//     }
+//   };
+
+//   const formFields = inputs
+//     .sort((a, b) => a.order - b.order)
+//     .map(
+//       ({ type, header, description, required, options, id }, idx: number) => {
+//         if (type === "checkbox") {
+//           const dataCheckboxOptions = options?.map((option: any) => {
+//             return { label: option, name: option, value: false };
+//           });
+
+//           return (
+//             <CheckboxGroupField
+//               key={idx}
+//               label={header}
+//               required={required}
+//               description={description}
+//               name={id!}
+//               options={dataCheckboxOptions}
+//               control={control}
+//               errorMsg={errors}
+//             />
+//           );
+//         } else if (type === "singleSelect") {
+//           const dataRadioOoptions = options?.map((option: string) => {
+//             return { label: option, value: option };
+//           });
+
+//           return (
+//             <RadioGroupField
+//               key={idx}
+//               name={id!}
+//               label={header}
+//               description={description}
+//               required={required}
+//               options={dataRadioOoptions}
+//               errorMsg={errors}
+//               optionClass="flex w-fit px-4 mb-1 justify-center items-center border rounded-lg py-2 cursor-pointer hover:bg-gray-100 data-[checked=true]:bg-blue-500 data-[checked=true]:text-white"
+//             />
+//           );
+//         } else if (type === "superText") {
+//           const dataInputTextarea = [
+//             {
+//               label: header,
+//               name: id!,
+//               placeholder: "Odpowiedź",
+//               description,
+//               required,
+//             },
+//           ];
+
+//           return (
+//             <TextareaFields
+//               key={idx}
+//               inputsData={dataInputTextarea}
+//               register={register}
+//               errorMsg={errors}
+//             />
+//           );
+//         } else {
+//           let placeholder;
+
+//           switch (type) {
+//             case "text": {
+//               placeholder = "Odpowiedź";
+//               break;
+//             }
+//             case "number": {
+//               placeholder = "Numer";
+//               break;
+//             }
+//             case "email": {
+//               placeholder = "Email";
+//               break;
+//             }
+//           }
+
+//           const dataInputText = [
+//             {
+//               label: header,
+//               name: id!,
+//               placeholder,
+//               type,
+//               description,
+//               required,
+//             },
+//           ];
+
+//           return (
+//             <InputFields
+//               key={idx}
+//               inputsData={dataInputText}
+//               register={register}
+//               errorMsg={errors}
+//               onChange={(name, v) => {
+//                 console.log(name, v);
+//               }}
+//             />
+//           );
+//         }
+//       }
+//     );
+
+//   return (
+//     <div className="flex justify-center ">
+//       <div className="w-4/5">
+//         <h1 className="text-4xl">{title}</h1>
+//         {description && <h2 className="text-2xl">{description}</h2>}
+//         <div className="text-red-600 text-sm mb-6">* Odpowiedź wymagana</div>
+
+//         <FormProvider {...methods}>
+//           <form
+//             onSubmit={handleSubmit(onSubmit)}
+//             className="w-4/5 bg-zinc-100 p-4"
+//           >
+//             {formFields}
+
+//             {/* {!isPreview && ( */}
+//             <Button message="Zatwierdź" disabled={false} type="submit" />
+//             {/* )} */}
+//           </form>
+//         </FormProvider>
+//         {/* <div className="w-fit ml-auto">
+//           <Button message="Opublikuj" type="button" />
+//         </div> */}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CreatedForm;
+
 "use client";
 
-import { SubmitForm } from "@/actions/form/SubmitForm";
 import {
   Button,
   CheckboxGroupField,
@@ -8,28 +243,25 @@ import {
   RadioGroupField,
   TextareaFields,
 } from "@/components/shared";
-import { useToast } from "@/hooks/useToast";
-import { uniqueErrorMessage } from "@/lib/error";
 import { createdFormSchema } from "@/lib/zodSchema/createdFormSchema";
 import { FormSerialized } from "@/types/form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { FormInput } from "lucide-react";
-import { ObjectId } from "mongodb";
 import { useEffect } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 
 type Props = {
   form: FormSerialized;
-  isPreview?: boolean
+  isPreview?: boolean;
 };
 
 const CreatedForm = (props: Props) => {
-  const { form: { title, description, inputs }, isPreview = false } = props;
+  const { title, description, inputs } = props.form;
   const schema = createdFormSchema(props.form.inputs);
-  const { toast } = useToast();
 
-  const defaultValues = inputs.reduce((acu: any, input) => {
-    const { header, type, options, id } = input;
+  // console.log("", inputs);
+
+  const defaultValues = inputs.reduce((acu: any, input: any) => {
+    const { header, type, options } = input;
     const inputLabel = header;
 
     const inputOptions = options.reduce(
@@ -43,6 +275,7 @@ const CreatedForm = (props: Props) => {
 
     const checkboxOptions = (acu[inputLabel] =
       type === "checkbox" ? inputOptions : "");
+
     return acu;
   }, {});
 
@@ -63,6 +296,8 @@ const CreatedForm = (props: Props) => {
     handleSubmit,
   } = methods;
 
+  // console.log("er", errors);
+
   // useEffect(() => {
   //   const subscription = watch((values) => {
   //     console.log("Aktualne wartości:", values);
@@ -70,40 +305,16 @@ const CreatedForm = (props: Props) => {
   //   return () => subscription.unsubscribe();
   // }, [watch]);
 
-  const onSubmit = async (data: any) => {
-    const _id = props.form._id?.toString()
-    if (!_id) return // ?
+  const onSubmit = (data: any) => {
+    console.log("sss", data);
+  };
 
-    // TODO: czemu trafiają tu Labele/Headery? Usunąć z obiektu i usunąć ten kod
-    const keys = Object.keys(data)
-    const inputIds = inputs.map(({ id }) => id)
-    keys.forEach((key) => {
-      if (!inputIds.includes(key)) delete data[key]
-    })
-
-    try {
-      await SubmitForm(_id, data)
-      toast({
-        title: 'Sukces! Dzieki',
-        variant: "success",
-      });
-    } catch(e) {
-      console.log('blad ', e)
-      const err = e as Error
-      const title = err.message == uniqueErrorMessage
-        ? 'Formularz z podanymi danymi zostal juz wyslany. Skontaktuj sie z administratorem.'
-        : 'Blad. Sprobuj ponownie.'
-      toast({
-        title,
-        variant: "error",
-      });
-    }
-  }
+  // console.log("", props.form);
 
   const formFields = inputs
-    .sort((a, b) => a.order - b.order)
+    .sort((a: any, b: any) => a.order - b.order)
     .map(
-      ({ type, header, description, required, options, id }, idx: number) => {
+      ({ type, header, description, required, options }: any, idx: number) => {
         if (type === "checkbox") {
           const dataCheckboxOptions = options?.map((option: any) => {
             return { label: option, name: option, value: false };
@@ -115,21 +326,21 @@ const CreatedForm = (props: Props) => {
               label={header}
               required={required}
               description={description}
-              name={id!}
+              name={header}
               options={dataCheckboxOptions}
               control={control}
               errorMsg={errors}
             />
           );
         } else if (type === "singleSelect") {
-          const dataRadioOoptions = options?.map((option: string) => {
+          const dataRadioOoptions = options?.map((option: any) => {
             return { label: option, value: option };
           });
 
           return (
             <RadioGroupField
               key={idx}
-              name={id!}
+              name={header}
               label={header}
               description={description}
               required={required}
@@ -142,7 +353,7 @@ const CreatedForm = (props: Props) => {
           const dataInputTextarea = [
             {
               label: header,
-              name: id!,
+              name: header,
               placeholder: "Odpowiedź",
               description,
               required,
@@ -178,7 +389,7 @@ const CreatedForm = (props: Props) => {
           const dataInputText = [
             {
               label: header,
-              name: id!,
+              name: header,
               placeholder,
               type,
               description,
@@ -192,7 +403,6 @@ const CreatedForm = (props: Props) => {
               inputsData={dataInputText}
               register={register}
               errorMsg={errors}
-              onChange={(name, v) => { console.log(name, v) }}
             />
           );
         }
@@ -213,12 +423,9 @@ const CreatedForm = (props: Props) => {
           >
             {formFields}
 
-            {!isPreview && <Button message="Zatwierdź" disabled={false} type="submit" />}
+            <Button message="Zatwierdź" disabled={false} type="submit" />
           </form>
         </FormProvider>
-        {/* <div className="w-fit ml-auto">
-          <Button message="Opublikuj" type="button" />
-        </div> */}
       </div>
     </div>
   );
--- src/lib/zodSchema/createdFormSchema.ts
+++ src/lib/zodSchema/createdFormSchema.ts
@@ -4,30 +4,70 @@ export const createdFormSchema = (inputs: any[]) => {
   const shape: Record<string, any> = {};
 
   inputs.forEach((input) => {
-    const fieldName = input.id;
+    const fieldName = input.header;
 
     switch (input.type) {
+      case "text":
+        shape[fieldName] = input.required
+          ? z.string().trim().min(2, "Min. 2 znaki").max(60, "Maks. 60 znaków")
+          : z.string().optional();
+        break;
+      case "superText":
+        shape[fieldName] = input.required
+          ? z
+              .string()
+              .trim()
+              .min(2, "Min. 2 znaki")
+              .max(200, "Maks. 200 znaków")
+          : z.string().optional();
+        break;
+
+      case "number":
+        shape[fieldName] = input.required
+          ? z.string().trim().regex(/\d/, "Min. 1 cyfra")
+          : z.string().optional();
+        break;
+
       case "checkbox":
         shape[fieldName] = input.required
           ? z
-            .record(z.boolean())
-            .refine(
-              (obj) => obj && Object.values(obj).some((v) => v),
-              "Wybierz co najmniej jedną opcję"
-            )
+              .record(z.boolean())
+              .refine(
+                (obj) => obj && Object.values(obj).some((v) => v),
+                "Wybierz co najmniej jedną opcję"
+              )
           : z.record(z.boolean()).optional();
         break;
 
       case "singleSelect":
         shape[fieldName] = input.required
           ? z
-            .string()
-            .nullable()
-            .refine(
-              (val) => val !== null && val !== "",
-              "Wybierz jedną opcję"
-            )
+              .string()
+              .nullable()
+              .refine(
+                (val) => val !== null && val !== "",
+                "Wybierz jedną opcję"
+              )
           : z.string().nullable().optional();
+        break;
+
+      case "email":
+        shape[fieldName] = input.required
+          ? z
+              .string()
+              .email("Podaj format email")
+              .nullable()
+              .refine((val) => val !== null && val !== "", "Pole wymagane")
+          : z.string().optional();
+        break;
+
+      case "date":
+        shape[fieldName] = input.required
+          ? z
+              .string()
+              .nullable()
+              .refine((val) => !isNaN(Date.parse(val!)), "Podaj datę")
+          : z.string().optional();
         break;
 
       default:
