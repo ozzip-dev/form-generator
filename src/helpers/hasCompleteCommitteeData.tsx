a6baefee548c201c4634b4219b8abe31b3881b89
pawelwier <pawiercinski@gmail.com>
2025-11-28 18:28:48 +0100

    fix hasCompleteCommitteeData condition

--- src/helpers/hasCompleteCommitteeData.tsx
+++ src/helpers/hasCompleteCommitteeData.tsx
@@ -9,8 +9,6 @@ export function hasCompleteCommitteeData(user: any): boolean {
   return requredCommitteeKeys.every((key) => {
     const value = user[key];
     return (
-      value !== undefined &&
-      value !== null &&
       typeof value === "string" &&
       value.trim().length > 0
     );
