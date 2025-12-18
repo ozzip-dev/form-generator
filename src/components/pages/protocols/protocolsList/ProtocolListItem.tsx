4e0baa43e0f7e6ce646974ee2c5a74fa00a6a486
krzysztofe <gitaragra@gmai.com>
2025-12-18 18:15:15 +0100

    Remove console.log from ProtocolListItemDetails

--- src/components/pages/protocols/protocolsList/ProtocolListItem.tsx
+++ src/components/pages/protocols/protocolsList/ProtocolListItem.tsx
@@ -1,23 +1,23 @@
 "use client";
 
-import {
-  useActionState,
-  useState,
-  startTransition,
-  SetStateAction,
-  Dispatch,
-  useEffect,
-} from "react";
+import { formatDateAndTime } from "@/helpers/dates/formatDateAndTime";
 import {
   ProtocolSerialized,
   ProtocolWithFilesSerialized,
 } from "@/types/protocol";
-import { formatDateAndTime } from "@/helpers/dates/formatDateAndTime";
 import Link from "next/link";
+import {
+  Dispatch,
+  SetStateAction,
+  startTransition,
+  useActionState,
+  useEffect,
+  useState,
+} from "react";
 
-import { Button, FullscreenLoader } from "@/components/shared";
-import ProtocolListItemDetails from "./ProtocolListItemDetails";
 import { getProtocolDetailsAction } from "@/actions/protocol/getProtocolDetails.Action";
+import { Button } from "@/components/shared";
+import ProtocolListItemDetails from "./ProtocolListItemDetails";
 
 type Props = {
   setPending: Dispatch<SetStateAction<boolean>>;
@@ -28,7 +28,7 @@ const ProtocolListItem = ({ setPending, protocol }: Props) => {
   const [details, setDetails] = useState<ProtocolWithFilesSerialized | null>(
     null
   );
-  const [_state, fetchDetails, isPending] = useActionState(async () => {
+  const [_, fetchDetails, isPending] = useActionState(async () => {
     if (details) {
       setDetails(null);
       return null;
