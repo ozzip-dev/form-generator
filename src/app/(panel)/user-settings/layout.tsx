61a9004444e314fda64716f7d299222e7e612d97
krzysztofe <gitaragra@gmai.com>
2026-01-03 18:26:27 +0100

    Add gear icon to settings

--- src/app/(panel)/user-settings/layout.tsx
+++ src/app/(panel)/user-settings/layout.tsx
@@ -5,8 +5,8 @@ type Props = { children: React.ReactNode };
 export default async function SettingsLayout(props: Props) {
   return (
     <div className="h-full flex flex-col">
-      <div className="shrink-0 container py-10">
-        <Icon icon="edit-form" size={47} color="var(--color-accent_opacity)" />
+      <div className="shrink-0 container py-8">
+        <Icon icon="gear" size={47} color="var(--color-accent_opacity)" />
       </div>
 
       <section className="flex-1 overflow-y-auto">{props.children}</section>
