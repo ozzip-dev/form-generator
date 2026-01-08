9c592424564829fe41381f4951f8bd7299d8f6b7
krzysztofe <gitaragra@gmai.com>
2026-01-08 19:15:22 +0100

    Add key to ResponsiveList

--- src/components/shared/responsiveList/ResponsiveList.tsx
+++ src/components/shared/responsiveList/ResponsiveList.tsx
@@ -7,7 +7,10 @@ const ResponsiveList = (props: Props) => {
     <ul className="md:flex md:flex-row md:w-4/5">
       {Object.entries(props.listItems).map((item) => {
         return (
-          <li className="flex  md:flex-1 min-w-0 mb-2 md:mb-0">
+          <li
+            key={JSON.stringify(item)}
+            className="flex  md:flex-1 min-w-0 mb-2 md:mb-0"
+          >
             <div className="w-2/5 md:hidden font-bold">{item[0]}</div>
             <div className="pr-2 truncate w-3/5 md:w-full">{item[1]}</div>
           </li>
