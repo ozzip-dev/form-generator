1acd8dc256b8540b13d9cb87385839ce3d1cff67
krzysztofe <gitaragra@gmai.com>
2026-01-15 11:22:25 +0100

    Change  bacground in CreatedFormTopBar

--- src/components/pages/form/CreatedFormTopBar.tsx
+++ src/components/pages/form/CreatedFormTopBar.tsx
@@ -5,7 +5,7 @@ type Props = {
 const CreatedFormTopBar = (props: Props) => {
   return (
     <div
-      className={`w-screen z-20 py-8 bg-accent_light 
+      className={`w-screen z-20 py-8 bg-accent 
     text-error text-center absolute left-0 -top-[6.5rem]
   transition-transform duration-700 ease-in-out z-20
    ${props.isError ? "translate-y-[6.5rem]" : "translate-y-0"}
