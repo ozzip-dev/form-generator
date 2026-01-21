d587bc0abfd6389c2f4de65f143ad03f174a531d
pawelwier <pawiercinski@gmail.com>
2026-01-21 22:19:20 +0100

    add avtive form list on moderator dashboard

--- src/components/pages/dashboard/DashboardModerator.tsx
+++ src/components/pages/dashboard/DashboardModerator.tsx
@@ -1,11 +1,13 @@
 import { requireUser } from "@/services/user-service";
+import ActiveForms from "./dashboard-moderator/ActiveForms";
 
 const DashboardModerator = async () => {
   const user = await requireUser();
 
   return (
-    <div className="m-4">
+    <div className="m-8">
       Witaj, <span className="font-black">{user.name}</span>
+      <ActiveForms />
     </div>
   );
 };
