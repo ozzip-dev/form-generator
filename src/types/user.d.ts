d4652154defa6b3e10999b9996c0f605d10a19e7
pawelwier <pawiercinski@gmail.com>
2026-01-27 18:08:09 +0100

    add user privacy field, privacy page, action, mongo fn

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -2,17 +2,22 @@ import { UserRole } from "@/models/User";
 import { User } from "better-auth";
 import { Document } from "mongodb";
 
-export type CommitteeInfoKey = 'committeeName' | 'committeeEmail' | 'committeePhone' | 'committeeUnion'
+export type CommitteeInfoKey =
+  | "committeeName"
+  | "committeeEmail"
+  | "committeePhone"
+  | "committeeUnion";
 
-export type UserCommitteeInfo = Record<CommitteeInfoKey, string>
+export type UserCommitteeInfo = Record<CommitteeInfoKey, string>;
 
-type UserWithCommittee = User & UserCommitteeInfo
+type UserWithCommittee = User & UserCommitteeInfo;
 
 // TODO Pawel: infer addition fields globally, edit IUser => User
 export interface IUser extends Document, UserWithCommittee {
   role: UserRole | string;
+  privacyPolicyConfirmed: boolean;
 }
 
-export interface UserSerialized extends Omit<IUser, '_id'> {
-  _id: string
+export interface UserSerialized extends Omit<IUser, "_id"> {
+  _id: string;
 }
