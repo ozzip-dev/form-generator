fb6d02b30d354cb0469d3d481fd0879512d6e7e9
krzysztofe <gitaragra@gmai.com>
2026-01-28 13:03:40 +0100

    const FormDescription = (props: Props) => { Add DOMPurify.sanititize to

--- src/components/shared/inputs/FormDescription.tsx
+++ src/components/shared/inputs/FormDescription.tsx
@@ -1,3 +1,5 @@
+import DOMPurify from "dompurify";
+
 type Props = {
   description?: string;
   variant: "edit" | "published"
@@ -6,13 +8,15 @@ type Props = {
 const FormDescription = (props: Props) => {
   if (!props.description) return null;
 
-const styles = props.variant === "edit" ? "mb-1 p-2 text-sm border rounded-sm [&_a]:text-accent_dark [&_a]:underline[&_h3]:text-lg" : 
-"[&_a]:text-accent_dark [&_a]:underline [&_h3]:text-lg my-2"
+  console.log('', props.description)
+
+  const styles = props.variant === "edit" ? "mb-1 p-2 text-sm border rounded-sm [&_a]:text-accent_dark [&_a]:underline[&_h3]:text-lg" :
+    "[&_a]:text-accent_dark [&_a]:underline [&_h3]:text-lg my-2"
 
   return (
     <div
       className={styles}
-      dangerouslySetInnerHTML={{ __html: props.description }}
+      dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(props.description) }}
     />
   );
 };
