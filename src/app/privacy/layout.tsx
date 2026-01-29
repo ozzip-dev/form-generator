5bd4f3b32924197a8355029d6f699a933bf534fd
pawelwier <pawiercinski@gmail.com>
2026-01-29 23:10:42 +0100

    fix privacy start view

--- src/app/privacy/layout.tsx
+++ src/app/privacy/layout.tsx
@@ -4,20 +4,20 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function DashboardLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    const session = await auth.api.getSession({ headers: await headers() });
-    if (!session) redirect("/login");
+  const session = await auth.api.getSession({ headers: await headers() });
+  if (!session) redirect("/login");
 
-    return (
-        <>
-            <header className="bg-accent flex justify-end p-6">
-                <LogoutButton />
-            </header>
+  return (
+    <>
+      <header className="bg-accent flex justify-end p-6">
+        <LogoutButton />
+      </header>
 
-            <main className="flex-1 overflow-y-auto">{children}</main>
-        </>
-    );
+      <main className="h-full pb-lg overflow-y-auto">{children}</main>
+    </>
+  );
 }
