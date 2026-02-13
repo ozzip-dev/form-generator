91988d6e745e042bf2aed15fb1fecec7829fa218
krzysztofe <gitaragra@gmai.com>
2026-02-13 18:47:18 +0100

    Add aspect ratio to img in Created form

--- src/components/pages/form/created-form/CreatedFormTopImage.tsx
+++ src/components/pages/form/created-form/CreatedFormTopImage.tsx
@@ -7,7 +7,7 @@ type Props = {
 const CreatedFormTopImage = ({ headerFileData }: Props) => {
   return (
     <div className="mb-8 flex w-full justify-center">
-      <div className="relative h-[16rem] w-full overflow-hidden rounded-md border md:rounded-lg">
+      <div className="relative aspect-[16/6] max-h-[16rem] w-full overflow-hidden rounded-md border md:rounded-lg">
         <Image
           src={`data:image/png;base64,${headerFileData}`}
           alt="Nagłówek formularza"
