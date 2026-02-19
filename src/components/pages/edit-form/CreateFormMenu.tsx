4f2f708b3fd6316a0943b9e92c1435f58788f108
pawelwier <pawiercinski@gmail.com>
2026-02-19 21:49:08 +0100

    add results tab for published forms only

--- src/components/pages/edit-form/CreateFormMenu.tsx
+++ src/components/pages/edit-form/CreateFormMenu.tsx
@@ -1,11 +1,16 @@
 import { NavMenu } from "@/components/shared/nav-menu";
+import { isActive } from "@/helpers/formHelpers";
+import { getFormById } from "@/services/form-service";
 import { NavMenuLink } from "@/types/shared";
 
 type Props = {
   formId: string;
 };
 
-const CreateFormMenu = ({ formId }: Props) => {
+const CreateFormMenu = async ({ formId }: Props) => {
+  const form = await getFormById(formId);
+  const isFormActive = isActive(form);
+
   const dataNavLinks: NavMenuLink[] = [
     { text: "Lista", link: `/forms` },
     { text: "Edycja", link: `/forms/${formId}/edit` },
@@ -13,9 +18,18 @@ const CreateFormMenu = ({ formId }: Props) => {
     { text: "Kontakty", link: `/forms/${formId}/contacts`, sameTab: false },
   ];
 
+  const resultsLink: NavMenuLink = {
+    text: "Wyniki",
+    link: `/forms/${formId}/results/details`,
+  };
+
+  const visibleLinks: NavMenuLink[] = isFormActive
+    ? [...dataNavLinks, resultsLink]
+    : dataNavLinks;
+
   return (
     <div className="py-8">
-      <NavMenu links={dataNavLinks} icon="edit-form" depth={3} level="sub" />
+      <NavMenu links={visibleLinks} icon="edit-form" depth={3} level="sub" />
     </div>
   );
 };
