7b4da426c56aafb3582cf69f5904b892678d4a92
pawelwier <pawiercinski@gmail.com>
2026-05-31 17:28:46 +0200

    home page edits 3

--- src/components/pages/home/sections/FormTemplates.tsx
+++ src/components/pages/home/sections/FormTemplates.tsx
@@ -7,7 +7,8 @@ import { TemplateFormId } from "@/lib/mongo/models";
 const FormTemplates = async () => {
   const templateForms = await getFormTemplates();
 
-  // Gdy wszystkie będą gotowe, trzymać się konwencji, usunąć i po id
+  // TODO Gdy wszystkie będą gotowe, trzymać się konwencji, usunąć i po id
+  // moze inaczej niz to pageItems nizej
   const templateImages: Record<TemplateFormId, string> = {
     [TemplateFormId.SURVEY]: "survey",
     [TemplateFormId.SIP]: "sip",
@@ -18,6 +19,13 @@ const FormTemplates = async () => {
     [TemplateFormId.LEAFLETING]: "leafleting",
   };
 
+  const pageItems: (Form | Partial<Form>)[] = [
+    ...templateForms,
+    {
+      templateTitle: "Twój własny formularz",
+    },
+  ];
+
   return (
     <section className="bg-font_dark text-white">
       <div className="container pb-20 pt-16">
@@ -26,31 +34,35 @@ const FormTemplates = async () => {
           subheader="Wybierz gotowy formularz i dostosuj go do swojej organizacji."
         />
 
-        <div className="mt-16 grid grid-cols-2 gap-10 sm:grid-cols-4 sm:justify-center">
-          {templateForms.map(({ _id, id, templateTitle }, idx) => {
+        <div className="mt-16 grid grid-cols-1 gap-10 sm:grid-cols-2 sm:justify-center lg:grid-cols-4">
+          {pageItems.map(({ _id, id, templateTitle }, idx) => {
             return (
               <div className="" key={idx}>
                 <div className="relative mx-auto mb-6 flex items-center justify-center rounded-sm text-font_dark">
                   <Image
-                    src={`/images/${templateImages[id]}.svg`}
+                    src={`/images/${id ? templateImages[id] : "survey"}.svg`}
                     alt=""
-                    width={0}
-                    height={0}
+                    width={300}
+                    height={250}
                     className="h-auto w-[300px]"
                   />
 
-                  <div className="sm:text-md absolute inset-0 mt-auto flex items-end justify-center text-lg md:text-base lg:text-lg">
-                    <p className="h-1/3 text-center">{templateTitle}</p>
+                  <div className="absolute inset-0 mt-auto flex items-end justify-center text-lg">
+                    <p className="h-[30%] px-4 text-center leading-8">
+                      {templateTitle}
+                    </p>
                   </div>
                 </div>
 
-                <ButtonLink
-                  target="_blank"
-                  message="Zobacz"
-                  link={`/${_id}`}
-                  variant="primary-rounded"
-                  className="mx-auto my-12 w-fit !border-white !bg-white !text-font_dark hover:!border-font_dark hover:!bg-font_dark hover:!text-white"
-                />
+                {!!_id && (
+                  <ButtonLink
+                    target="_blank"
+                    message="Zobacz"
+                    link={`/${_id}`}
+                    variant="primary-rounded"
+                    className="mx-auto my-12 w-fit !border-white !bg-white !text-font_dark hover:!border-font_dark hover:!bg-font_dark hover:!text-white"
+                  />
+                )}
               </div>
             );
           })}
