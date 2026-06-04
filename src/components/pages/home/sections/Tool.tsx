3835805440cdee7a384a4d46be3c060d5a8a964c
pawelwier <pawiercinski@gmail.com>
2026-06-04 06:49:18 +0200

    home: tool section refactor

--- src/components/pages/home/sections/Tool.tsx
+++ src/components/pages/home/sections/Tool.tsx
@@ -2,6 +2,26 @@ import Image from "next/image";
 import SectionHeader from "../SectionHeader";
 
 const Tool = () => {
+  // Pawel: na razie zostawiam stare ikony w .svg w /public
+  const toolData: { icon: string; text: string }[] = [
+    {
+      icon: "form",
+      text: "Twórz formularze w kilka minut",
+    },
+    {
+      icon: "results-view",
+      text: "Wyniki w formie tabel i wykresów",
+    },
+    {
+      icon: "results-export",
+      text: "Eksportuj wyniki w formatach .xls i .pdf",
+    },
+    {
+      icon: "gear",
+      text: "Dla organizacji związkowych",
+    },
+  ];
+
   return (
     <section className="container">
       <div className="container pb-20 pt-16">
@@ -12,63 +32,19 @@ const Tool = () => {
             zbieranie opinii, organizowanie głosowań i wyborów online."
         />
 
-        <div className="mt-16 gap-10 sm:flex sm:flex-wrap sm:justify-center">
-          <div className="sm:w-[180px]">
-            <div className="mx-auto mb-6 flex h-[70px] w-[70px] items-center justify-center rounded-[2rem] bg-error">
+        <div className="mt-16 flex flex-wrap justify-center gap-10">
+          {toolData.map(({ icon, text }, idx) => (
+            <div key={idx} className="w-[160px] sm:w-[180px]">
               <Image
-                src="/images/form.svg"
+                src={`/images/tools/${icon}.png`}
                 alt=""
-                width={0}
-                height={0}
-                className="h-auto w-[30px]"
+                width={70}
+                height={70}
+                className="mx-auto mb-6"
               />
+              <p className="text-center text-sm">{text}</p>
             </div>
-            <p className="text-center text-sm">
-              Twórz formularze w kilka minut
-            </p>
-          </div>
-
-          <div className="sm:w-[180px]">
-            <div className="mx-auto mb-6 flex h-[70px] w-[70px] items-center justify-center rounded-[2rem] bg-error">
-              <Image
-                src="/images/table.svg"
-                width={0}
-                height={0}
-                alt=""
-                className="h-auto w-[40px]"
-              />
-            </div>
-            <p className="text-center text-sm">
-              Wyniki aktualizują się automatycznie
-            </p>
-          </div>
-
-          <div className="sm:w-[180px]">
-            <div className="mx-auto mb-6 flex h-[70px] w-[70px] items-center justify-center rounded-[2rem] bg-error">
-              <Image
-                src="/images/graph.svg"
-                alt=""
-                width={0}
-                height={0}
-                className="h-auto w-[40px]"
-              />
-            </div>
-            <p className="text-center text-sm">
-              Wyniki zapisuj w Exel, PDF, wykresy
-            </p>
-          </div>
-          <div className="sm:w-[180px]">
-            <div className="mx-auto mb-6 flex h-[70px] w-[70px] items-center justify-center rounded-[2rem] bg-error">
-              <Image
-                src="/images/gear.svg"
-                width={0}
-                height={0}
-                alt=""
-                className="h-auto w-[40px]"
-              />
-            </div>
-            <p className="text-center text-sm">Dla organizacji związkowych</p>
-          </div>
+          ))}
         </div>
       </div>
     </section>
