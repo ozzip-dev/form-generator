1e48738de0a3aa2033eb39b1afa1dd87b62693c8
pawelwier <pawiercinski@gmail.com>
2026-07-09 22:26:47 +0200

    home: footer font color

--- src/components/pages/home/sections/ContactDetails.tsx
+++ src/components/pages/home/sections/ContactDetails.tsx
@@ -1,7 +1,7 @@
 const ContactDetails = () => {
   return (
     <div className="container p-4 text-white">
-      <div className="flex gap-4 text-2xs text-font_light">
+      <div className="flex gap-4 text-2xs text-font_dark">
         <div>
           <p>OZZ Inicjatywa Pracownicza</p>
           <p>ul. Kościelna 4/1a, 60-538 Poznań</p>
